7a5958db3cd1440c076afa3db8130b43baa8abde
churongcon114-lab <churongcon114@gmail.com>
2026-01-25 22:40:26 +0700

    feature/ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+console.log("VITE ENV TEST:", {
+  API: import.meta.env.VITE_API_URL,
+  EMAILJS_SERVICE: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+});
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
